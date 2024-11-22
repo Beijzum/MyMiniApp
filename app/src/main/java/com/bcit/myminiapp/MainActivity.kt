a67ba7c0d6310741a2e14f6ce35dccb1916ac5d2
Jason Chow <jchow149@my.bcit.ca>
2024-11-22 11:04:34 -0800

    ADD MainContent

--- app/src/main/java/com/bcit/myminiapp/MainActivity.kt
+++ app/src/main/java/com/bcit/myminiapp/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import com.bcit.myminiapp.data.ArtRepository
 import com.bcit.myminiapp.data.Endpoints
 import com.bcit.myminiapp.ui.state.ArtState
 
@@ -18,23 +20,27 @@ class MainActivity : ComponentActivity() {
         val artRepository = (application as MyApp).artRepository
 
         setContent {
+            MainContent(artRepository)
+        }
+    }
 
-            val artState = ArtState(artRepository)
+    @Composable
+    fun MainContent(artRepository: ArtRepository){
 
-            LaunchedEffect(artState) {
-                artState.getArtwork()
+        val artState = ArtState(artRepository)
+
+        LaunchedEffect(artState) {
+            artState.getArtwork()
+        }
+
+        LazyColumn {
+            items(artState.artwork.size) {
+                Text(artState.artwork[it].title, fontSize = 30.sp)
+                AsyncImage(
+                    model = Endpoints.IMAGE_ENDPOINT.format(artState.artwork[it].image),
+                    contentDescription = null
+                )
             }
-
-            LazyColumn {
-                items(artState.artwork.size) {
-                    Text(artState.artwork[it].title, fontSize = 30.sp)
-                    AsyncImage(
-                        model = Endpoints.IMAGE_ENDPOINT.format(artState.artwork[it].image),
-                        contentDescription = null
-                    )
-                }
-            }
-
         }
     }
 }
