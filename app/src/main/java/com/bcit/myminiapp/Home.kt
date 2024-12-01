2dbee8e3ec01b84c9c5ea78b0f93e86a511efb27
Beijzum <144724833+Beijzum@users.noreply.github.com>
2024-12-01 06:35:51 -0800

    ADD loading

--- app/src/main/java/com/bcit/myminiapp/Home.kt
+++ app/src/main/java/com/bcit/myminiapp/Home.kt
@@ -12,7 +12,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -23,11 +26,20 @@ import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import com.bcit.myminiapp.data.Endpoints
 import com.bcit.myminiapp.ui.state.PokemonState
+import kotlinx.coroutines.delay
 
 @Composable
 fun Home(navController: NavController) {
 
     var pokemonState: PokemonState = viewModel(navController.getBackStackEntry("home"))
+
+    val (isLoading, setLoading) = remember { mutableStateOf(true) }
+
+    LaunchedEffect(pokemonState.searchFlow.collectAsState().value) {
+        setLoading(true)
+        delay(8000)
+        setLoading(false)
+    }
 
     Column(
         Modifier.fillMaxSize(),
@@ -45,37 +57,54 @@ fun Home(navController: NavController) {
             }
         )
         Spacer(Modifier.height(16.dp))
-        LazyColumn(
-            Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            items(pokemonState.pokemon.size) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            navController.navigate("details/${pokemonState.pokemon[it].id}")
-                        }
-                ) {
-                    Text(
-                        pokemonState.pokemon[it].name,
-                        fontSize = 30.sp,
-                        modifier = Modifier.padding(bottom = 8.dp)
+
+        if (pokemonState.pokemon.isEmpty()) {
+            if (isLoading) {
+                Text(
+                    text = "Loading...",
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+            } else {
+                Text(
+                    text = "No Pokémon found",
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+            }
+        } else {
+            LazyColumn(
+                Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                items(pokemonState.pokemon.size) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                navController.navigate("details/${pokemonState.pokemon[it].id}")
+                            }
+                    ) {
+                        Text(
+                            pokemonState.pokemon[it].name,
+                            fontSize = 30.sp,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                    }
+                    AsyncImage(
+                        model = Endpoints.IMAGE_ENDPOINT.format(
+                            pokemonState.pokemon[it].id.replace("-", "/")
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(bottom = 30.dp)
+                            .clickable {
+                                navController.navigate("details/${pokemonState.pokemon[it].id}")
+                            }
                     )
                 }
-                AsyncImage(
-                    model = Endpoints.IMAGE_ENDPOINT.format(
-                        pokemonState.pokemon[it].id.replace("-", "/")
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .padding(bottom = 30.dp)
-                        .clickable {
-                            navController.navigate("details/${pokemonState.pokemon[it].id}")
-                        }
-                )
             }
         }
     }
