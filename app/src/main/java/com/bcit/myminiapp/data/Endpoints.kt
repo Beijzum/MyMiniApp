bee07257aac2b841f0d25a3fbb18ca3a79083f75
Beijzum <144724833+Beijzum@users.noreply.github.com>
2024-12-01 02:35:15 -0800

    FIX search

--- app/src/main/java/com/bcit/myminiapp/data/Endpoints.kt
+++ app/src/main/java/com/bcit/myminiapp/data/Endpoints.kt
@@ -2,7 +2,7 @@ package com.bcit.myminiapp.data
 
 enum class Endpoints(val url: String) {
     BASE_URL("https://api.pokemontcg.io/v2"),
-    SEARCH("${BASE_URL.url}/cards?name=%s&fields=id,name"),
+    SEARCH("${BASE_URL.url}/cards?q=name:%s*"),
     IMAGE_ENDPOINT("https://images.pokemontcg.io/%s.png");
 
     fun format(str: String?): String {
